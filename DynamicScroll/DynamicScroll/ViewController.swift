7d61e2c576d072362cd9b0239c18c410dccacc5e
seizze <cwcwc3@gmail.com>
2019-11-17 21:23:13 +0900

    DynamicScroll project init

---
+++ DynamicScroll/DynamicScroll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DynamicScroll
+//
+//  Created by Chaewan Park on 2019/11/17.
+//  Copyright © 2019 seizze. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
