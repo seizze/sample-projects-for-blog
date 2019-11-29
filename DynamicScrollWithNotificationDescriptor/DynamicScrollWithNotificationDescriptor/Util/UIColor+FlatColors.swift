8c04c97644b2f92215c56f576edfc978dfd851f0
seizze <cwcwc3@gmail.com>
2019-11-29 22:08:25 +0900

    Copy and rename project for notification descriptor

---
+++ DynamicScrollWithNotificationDescriptor/DynamicScrollWithNotificationDescriptor/Util/UIColor+FlatColors.swift
@@ -0,0 +1,57 @@
+//
+//  UIColor+FlatColors.swift
+//
+//  Created by Chaewan Park on 2019/11/17.
+//  Copyright © 2019 seizze. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    struct FlatColor {
+        struct Green {
+            static let Fern = UIColor(inHex: 0x6ABB72)
+            static let MountainMeadow = UIColor(inHex: 0x3ABB9D)
+            static let ChateauGreen = UIColor(inHex: 0x4DA664)
+            static let PersianGreen = UIColor(inHex: 0x2CA786)
+        }
+        
+        struct Blue {
+            static let PictonBlue = UIColor(inHex: 0x5CADCF)
+            static let Mariner = UIColor(inHex: 0x3585C5)
+            static let CuriousBlue = UIColor(inHex: 0x4590B6)
+            static let Denim = UIColor(inHex: 0x2F6CAD)
+            static let Chambray = UIColor(inHex: 0x485675)
+            static let BlueWhale = UIColor(inHex: 0x29334D)
+        }
+        
+        struct Violet {
+            static let Wisteria = UIColor(inHex: 0x9069B5)
+            static let BlueGem = UIColor(inHex: 0x533D7F)
+        }
+        
+        struct Yellow {
+            static let Energy = UIColor(inHex: 0xF2D46F)
+            static let Turbo = UIColor(inHex: 0xF7C23E)
+        }
+        
+        struct Orange {
+            static let NeonCarrot = UIColor(inHex: 0xF79E3D)
+            static let Sun = UIColor(inHex: 0xEE7841)
+        }
+        
+        struct Red {
+            static let TerraCotta = UIColor(inHex: 0xE66B5B)
+            static let Valencia = UIColor(inHex: 0xCC4846)
+            static let Cinnabar = UIColor(inHex: 0xDC5047)
+            static let WellRead = UIColor(inHex: 0xB33234)
+        }
+        
+        struct Gray {
+            static let AlmondFrost = UIColor(inHex: 0xA28F85)
+            static let WhiteSmoke = UIColor(inHex: 0xEFEFEF)
+            static let Iron = UIColor(inHex: 0xD1D5D8)
+            static let IronGray = UIColor(inHex: 0x75706B)
+        }
+    }
+}
